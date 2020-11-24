81f3cfdd7b1bce0776a252721ca321b67e34e196
stxrboi <stxrboii@outlook.com>
2020-11-24 22:04:27 +0200

    Updated Final Version

--- NewappClean.R
+++ NewappClean.R
@@ -10,13 +10,13 @@ library(DT)
 ui<- dashboardPage(
   dashboardHeader(title = "Crime Analytics"),
   dashboardSidebar(
-    selectInput(inputId ="classes_input", label="Select:", choices=c("CRIMES_AGAINST_THE _PERSON", "CONTACT_RELATED_CRIMES", "PROPERTY_RELATED_CRIMES", "OTHER_SERIOUS_CRIMES", "CRIME_DETECTED_AS_A_RESULT_OF_POLICE_ACTION", "SUBCATEGORIES_OF_AGGRAVATED_ROBBERY")), 
+    selectInput(inputId ="classes_input", label="Select class:", choices=c("CRIMES_AGAINST_THE _PERSON", "CONTACT_RELATED_CRIMES", "PROPERTY_RELATED_CRIMES", "OTHER_SERIOUS_CRIMES", "CRIME_DETECTED_AS_A_RESULT_OF_POLICE_ACTION", "SUBCATEGORIES_OF_AGGRAVATED_ROBBERY")), 
     
     sliderInput("TotalCrimes", "Select minimum Cases:", 100,500,100)
   ),
   dashboardBody(
     column(width = 7,
-           plotOutput("crimesperclass", height = 400)
+           plotOutput("CrimesPerClass", height = 400)
     ),
     column(width = 5,
            plotOutput("PlotTCrimes", height = 400)
@@ -27,12 +27,14 @@ ui<- dashboardPage(
 )
 server <- function(input, output){
   
-  wdf<-read.csv("crime_data.csv", sep=",")  #storing the wide data into a wide data frame (wdf)
+  wdf<-read.csv("crime_data.csv", sep=",")  #storing the data into a wide data frame (wdf)
   cl <- wdf$classes
+  wdf2<-wdf
+  wdf2$X=NULL
   
   
-  ldf<-melt(wdf,id.vars=c("classes","CRIME_CATEGORY"), measure.vars = c("April_2006_to_March_2007","April_2007_to_March_2008","April_2008_to_March_2009","April_2009_to_March_2010","April_2010_to_March_2011","April_2011_to_March_2012","April_2012_to_March_2013","April_2013_to_March_2014","April_2014_to_March_2015","April_2015_to_March_2016"),
-            variable.name="Years") #converting the wdf into a long data frame (ldf) to be able to plot the graphs
+  ldf<-melt(wdf2,id.vars=c("classes","CRIME_CATEGORY"), measure.vars = c("April_2006_to_March_2007","April_2007_to_March_2008","April_2008_to_March_2009","April_2009_to_March_2010","April_2010_to_March_2011","April_2011_to_March_2012","April_2012_to_March_2013","April_2013_to_March_2014","April_2014_to_March_2015","April_2015_to_March_2016"),
+            variable.name="Years") #converting the wdf2 into a long data frame (ldf) to be able to plot the graphs
   
   
   ldf$value[which(is.na(ldf$value))]<-0  #changing NA values to 0
@@ -43,8 +45,7 @@ server <- function(input, output){
   #we no more need it to be for both a specific year and class hence we're only calculating the values by year
   
   data_classes<-reactive({
-    df_1<-ldf_agg %>% filter(classes==input$classes_input) #%>% 
-    
+    df_1<-ldf_agg %>% filter(classes==input$classes_input)
     
     return(df_1)
   })
@@ -56,17 +57,15 @@ server <- function(input, output){
   })
   
   crimetable <- reactive({
-    df_3 <- wdf %>% filter(classes==input$classes_input)
+    df_3 <- wdf2 %>% filter(classes==input$classes_input)
     
     return(df_3)
   })
   
-  
-  output$crimesperclass<-renderPlot({
+  output$CrimesPerClass<-renderPlot({
     data_classes() %>%
       ggplot(aes(x=reorder(Years,value),y=value,fill=Years))+
-      geom_col()+labs(x=element_blank(),y="Total Crimes",
-                      title = "Crimes Statistics per Class")+
+      geom_col()+labs(x=element_blank(),y="Total Crimes",title = "Crimes Statistics per Class")+
       guides(fill=FALSE)+
       theme_bw()+
       theme(plot.title = element_text(hjust = 0.5))+
